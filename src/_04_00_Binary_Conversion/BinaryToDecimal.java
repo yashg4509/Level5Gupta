c676a3160af9b002ac88b17366ffb9ca8f5eb633
League Student <league@iMac.attlocal.net>
2019-06-25 18:31:56 -0700

    IP on BinaryToDecimal

--- src/_04_00_Binary_Conversion/BinaryToDecimal.java
+++ src/_04_00_Binary_Conversion/BinaryToDecimal.java
@@ -5,25 +5,37 @@ public class BinaryToDecimal {
 		// Binary numbers in Java can be represented with the '0b' prefix.
 		// For example:
 		System.out.println(0b111);
-		//This prints 7, because 111 is 7 in binary.
-		
-		//EXAMPLE: Convert 0 1 0 1 1 0 from binary to decimal
-		
-		/*To convert binary to decimal, use the following algorithm:
-			1. Start from the right most digit of the binary number and assign that bit the value of 1.
-		 	 	eg: 0	1	0	1	1	0
-		                      			1 
-			2. As you move to the left, assign the next digit a value that is double the previous digit.
-				eg: 0	1	0	1	1	0
-				    32	16	8	4	2	1
-				    
-			3. For every 1 in the binary number, add all of the corresponding values together.
-				eg: 0	1	0	1	1	0
-				    32	16	8	4	2	1
-				    
-				    16 + 4 + 2 = 22
-				    010110 in binary is equal to 22 in decimal!
-		 
+		// This prints 7, because 111 is 7 in binary.
+
+		// EXAMPLE: Convert 0 1 0 1 1 0 from binary to decimal
+		int num = 0b010110;
+		boolean[] convert = new boolean[8];
+
+		for (int i = 0; i < convert.length; i++) {
+			convert[i] = (num % 2 == 1);
+			num = num >> 1;
+			if (num == 0)
+				break;
+		}
+		int result = 0;
+		int rank = 1;
+		for (int i = 0; i < convert.length; i++) {
+			if (convert[i])
+				result += rank;
+			rank *= 2;
+		}
+		System.out.println(result);
+		/*
+		 * To convert binary to decimal, use the following algorithm: 1. Start from the
+		 * right most digit of the binary number and assign that bit the value of 1. eg:
+		 * 0 1 0 1 1 0 1 2. As you move to the left, assign the next digit a value that
+		 * is double the previous digit. eg: 0 1 0 1 1 0 32 16 8 4 2 1
+		 * 
+		 * 3. For every 1 in the binary number, add all of the corresponding values
+		 * together. eg: 0 1 0 1 1 0 32 16 8 4 2 1
+		 * 
+		 * 16 + 4 + 2 = 22 010110 in binary is equal to 22 in decimal!
+		 * 
 		 */
 	}
 }
--- src/_04_00_Binary_Conversion/DecimalToBinary.java
+++ src/_04_00_Binary_Conversion/DecimalToBinary.java
@@ -1,35 +1,88 @@
 package _04_00_Binary_Conversion;
 
-public class Decimal_To_Binary {
+import java.util.ArrayList;
+import java.util.List;
+
+public class DecimalToBinary {
+
+	public DecimalToBinary() {
+
+	}
+
 	public static void main(String[] args) {
-		//Converting a decimal number to binary is a little trickier.
-		
-		//EXAMPLE: Convert 43 to binary
-		
+		// Converting a decimal number to binary is a little trickier.
+		DecimalToBinary db = new DecimalToBinary();
+		// EXAMPLE: Convert 43 to binary
+		System.out.println(db.convert(43));
+
 		/*
-		 * Step 1: Start with one and add a digit the left that is double the value of the previous number.
-		 *         Stop when you've passed the target number        
-		 *         eg. 43
-		 *         	64	32	16	8	4	2	1
-		 *        
-		 *	Step 2: Remove the left most value (the one that is higher than the target). 
-		 *			eg. 43
-		 *         	32	16	8	4	2	1
-		 *         
-		 *  Step 3: Find the combination of values that add up to the target number.
-		 *  		eg. 43
-		 *         	32	16	8	4	2	1
-		 *          43 = 32 + 8 + 2 + 1
-		 *         
-		 *  Step 4: Every matching number in the list is a 1, and non-matching is a 0.
-		 *  		eg. 43
-		 *         	32	16	8	4	2	1
-		 *          43 = 32 + 8 + 2 + 1
-		 *         
-		 *          1    0   1   0   1   1
-		 *          32	16	 8	 4	 2	 1
-		 *         
-		 *         43 in decimal is 101011 in binary!
+		 * Step 1: Start with one and add a digit the left that is double the value of
+		 * the previous number. Stop when you've passed the target number eg. 43 64 32
+		 * 16 8 4 2 1
+		 * 
+		 * Step 2: Remove the left most value (the one that is higher than the target).
+		 * eg. 43 32 16 8 4 2 1
+		 * 
+		 * Step 3: Find the combination of values that add up to the target number. eg.
+		 * 43 32 16 8 4 2 1 43 = 32 + 8 + 2 + 1
+		 * 
+		 * Step 4: Every matching number in the list is a 1, and non-matching is a 0.
+		 * eg. 43 32 16 8 4 2 1 43 = 32 + 8 + 2 + 1
+		 * 
+		 * 1 0 1 0 1 1 32 16 8 4 2 1
+		 * 
+		 * 43 in decimal is 101011 in binary!
 		 */
+	}
+
+	private String convert(int t) {
+		List<Integer> l = build(t);
+		List<Integer> result = find(t, l, 0);
+		System.out.println(result);
+		return "";
+	}
+
+	private List<Integer> find(int t, List<Integer> l, int i) {
+		if (i >= l.size()) {
+			if (t == count(l)) {
+				return l;
+			} else {
+				return null;
+			}
+		}
+		List<Integer> list = find(t, l, i + 1);
+
+		if (list != null) {
+			return list;
+		}
+
+		int removed = l.remove(i);
+		list = find(t, l, i + 1);
+		if (list != null)
+			l.add(i, removed);
+		return list;
+	}
+
+	private int count(List<Integer> l) {
+		int sum = 0;
+
+		for (int i = 0; i < l.size(); i++) {
+			sum += l.get(i);
+		}
+
+		return sum;
+	}
+
+	private List<Integer> build(int t) {
+		int i = 1;
+
+		List<Integer> vals = new ArrayList<>();
+		while (i <= t) {
+			vals.add(i);
+			i *= 2;
+		}
+
+		return vals;
+
 	}
 }
