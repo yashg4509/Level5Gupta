9a4c672cb4b52fb3150bcc0b268978885e037077
League Student <league@iMac-22.attlocal.net>
2019-07-28 14:25:35 -0700

    Base64Decoder (IP)

--- src/_04_04_Base64_Decoder/Base64Decoder.java
+++ src/_04_04_Base64_Decoder/Base64Decoder.java
@@ -46,15 +46,30 @@ public class Base64Decoder {
 	// characters long and return an array of 3 bytes (24 bits). The byte
 	// array should be the binary value of the encoded characters.
 	public static byte[] convert4CharsTo24Bits(String s) {
-		byte[] arr = new byte[3];
+		byte[] res = new byte[3];
 
 		char[] charr = s.toCharArray();
+
+		res[0] = (byte) (convertBase64Char(charr[0]) << 2 + convertBase64Char(charr[1]) >> 4);
+		res[1] = (byte) (convertBase64Char(charr[1]) << 4 + convertBase64Char(charr[2]) >> 2);
+		res[2] = (byte) (convertBase64Char(charr[3]));
+
+		return res;
 
 	}
 
 	// 3. Complete this method so that it takes in a string of any length
 	// and returns the full byte array of the decoded base64 characters.
 	public static byte[] base64StringToByteArray(String file) {
-		return null;
+		byte[] arr = new byte[(file.length() / 4) * 3];
+		String input = "";
+
+		for (int i = 0; i < file.length(); i += 4) {
+			input = file.substring(i, i + 4);
+			arr = convert4CharsTo24Bits(file);
+		}
+
+		return arr;
+
 	}
 }
